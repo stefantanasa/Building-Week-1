1f9ca46227362e5d26b8454d5e396e5db6a28db5
Bokijonov Mukhsinjon <78298971+BokijonovM@users.noreply.github.com>
2021-11-22 18:29:32 +0100

    h

--- album/album.js
+++ album/album.js
@@ -1,26 +1,11 @@
 
 
 //
-/*
+
 function changeMusicNumberToIcon() {
     let a = document.getElementById("id-music-tabel")
-    let numberClass = a.getElementsByClassName("table-number")
+    let numberClass = a.getElementsByClassName("heart-music-icon")
     for (let i = 0; i < numberClass.length; i++) {
-        numberClass[i].style.display = "none";
-        let iconClass = a.getElementsByClassName("play-btn-musics")
-        for (let i = 0; i < iconClass.length; i++) {
-            iconClass[i].style.display = "flex";
-        }
-    }
-
-
-
-}
-function addIcon() {
-    let a = document.getElementById("id-music-tabel")
-    let iconClass = a.getElementsByClassName("heard-music")
-    for (let i = 0; i < iconClass.length; i++) {
-        iconClass[i].style.display = "flex";
+        numberClass[i].style.display = "flex";
     }
 }
-*/
